61472f040b95bdcd607dd7bcf79085e6de4ee034
Pranay Kr. Srivastava <pranay.srivastava@pantacor.com>
2019-12-11 21:25:02 +0530

    Add position markers to tailed files
    This patch does the following
    
    1. Makes pv logger wait when the parent directory of the file to be
       tailed is not present. Otherwise it'll fill up the logs very soon.
       The duration is set to be 5 seconds.
    
    2. Writes the amount of file position pushed to PH and written to the
       log file in a new XATTR of the file being tailed. If XATTRs are not
       supported then current behaviour of whole file being tailed from the
       beginning will be used.
    
    Signed-off-by: Pranay Kr. Srivastava <pranay.srivastava@pantacor.com>

--- pvlogger.h
+++ pvlogger.h
@@ -2,4 +2,12 @@
 #define __PV_LOGGER_H_
 #include "pantavisor.h"
 int start_pvlogger(struct pv_log_info *log_info, const char *platform);
+
+/*
+ * user.* attributes are available usually for most file systems.
+ * user. prefix is hence necessary! Otherwise filesystem(s) complaint
+ * of ENOTSUP.
+ * */
+#define PV_LOGGER_POS_XATTR 	"user.pv.logger.pos"
+#define PV_LOGGER_FILE_WAIT_TIMEOUT 	(5)
 #endif  /*__PV_LOGGER_H_*/
