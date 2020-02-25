cf675311ffb4e024a1ab6e4f75146f2c7b452311
Pranay Kr. Srivastava <pranay.srivastava@pantacor.com>
2020-02-25 15:52:32 +0530

    Change logger attribute to trusted
    Some file systems viz tmpfs do not support user attributes. Since we run
    as a priviliged containers it's ok to use "truseted.*" namespace. This
    is done to avoid having xattr errors when trying to save the current
    logger position as an xattr value with the file attributes.
    
    NOTE: When using overlayfs, the upper dir's file system must support the
    trusted xattr namespace and the container must be running as a
    priviliged container.
    
    TODO:
    For unprivliged containers the "trusted" xattr namespace may not work.
    
    Signed-off-by: Pranay Kr. Srivastava <pranay.srivastava@pantacor.com>

--- pvlogger.h
+++ pvlogger.h
@@ -8,6 +8,6 @@ int start_pvlogger(struct pv_log_info *log_info, const char *platform);
  * user. prefix is hence necessary! Otherwise filesystem(s) complaint
  * of ENOTSUP.
  * */
-#define PV_LOGGER_POS_XATTR 	"user.pv.logger.pos"
+#define PV_LOGGER_POS_XATTR 	"trusted.pv.logger.pos"
 #define PV_LOGGER_FILE_WAIT_TIMEOUT 	(5)
 #endif  /*__PV_LOGGER_H_*/
