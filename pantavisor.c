97423aa1ca5062c9601cca58713842fc7f165337
Aníbal Portero Hermida <anibal.portero@pantacor.com>
2020-10-02 13:26:07 +0200

    fix addons list warnings

--- pantavisor.c
+++ pantavisor.c
@@ -420,6 +420,8 @@ int pv_meta_link_boot(struct pantavisor *pv, struct pv_state *s)
 	int i;
 	struct pantavisor_config *c = pv->config;
 	char src[PATH_MAX], dst[PATH_MAX], fname[PATH_MAX], prefix[32];
+	struct pv_addon *a, *tmp;
+	struct dl_list *addons = NULL;
 
 	if (!s)
 		s = pv->state;
@@ -450,8 +452,7 @@ int pv_meta_link_boot(struct pantavisor *pv, struct pv_state *s)
 
 	// addons
 	i = 0;
-	struct pv_addon *a, *tmp;
-	struct dl_list *addons = &s->addons;
+	addons = &s->addons;
 	dl_list_for_each_safe(a, tmp, addons,
 			struct pv_addon, list) {
 		sprintf(dst, "%s/trails/%d/.pv/", c->storage.mntpoint, s->rev);
