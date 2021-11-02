d25e3b09b9021e78573c02a20d2a43902f01d5f8
Alexander Sack <asac@pantacor.com>
2021-11-02 11:07:17 +0100

    improve gen_version.sh to include git describe info instead of PANTAVSISOR_VERSION

--- gen_version.sh
+++ gen_version.sh
@@ -4,13 +4,14 @@ buildir=$2
 
 IFS=
 manifest=$(repo manifest -r)
+gitdescribe=$(cd .repo/manifests; git describe --tags)
 sha256=$(echo $manifest | sha256sum | awk '{print $1}')
 manifest=$(echo $manifest | sed 's/"/\\"/g' | sed -e '$ ! s/$/ \\n \\/')
 
 now=$(date +'%y%m%d')
 
 echo "const char *pv_build_manifest = \"${manifest}\";" > $buildir/version.c
-echo "const char *pv_build_version = \"${PANTAVISOR_VERSION}-${now}-${sha256:0:7}\";" >> $buildir/version.c
+echo "const char *pv_build_version = \"${gitdescribe}-${now}-${sha256:0:7}\";" >> $buildir/version.c
 echo "const char *pv_build_date = \"${now}\";" >> $buildir/version.c
 echo "const char *pv_build_arch = \"${3}\";" >> $buildir/version.c
 
