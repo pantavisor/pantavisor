b5a216a7dac9745d7bec2e281dc5d443c7c9215e
Felipe Ortiz <felipe.ortiz@pantacor.com>
2022-06-04 12:33:24 -0400

    Remove unused function

--- utils/filesystem.c
+++ utils/filesystem.c
@@ -39,26 +39,6 @@ bool pv_filesystem_path_is_directory(const char *path)
     return false;
 }
 
-static int get_directory(char *dir, const char *path)
-{
-    if (!pv_filesystem_path_exist(path)) {
-        dir = NULL;
-        return -1;
-    }
-
-    if (pv_filesystem_path_is_directory(path)) {
-        strncpy(dir, path, strnlen(path, PATH_MAX));
-        return 0;
-    } else if (errno == ENOTDIR) {
-        char copy[PATH_MAX];
-        strncpy(copy, path, strnlen(path, PATH_MAX));
-
-        char *tmp = dirname(copy);
-        strncpy(dir, tmp, strnlen(path, PATH_MAX));
-    }
-    return 0;
-}
-
 void pv_filesystem_path_sync(const char *path)
 {
     char dir[PATH_MAX] = { 0 };
