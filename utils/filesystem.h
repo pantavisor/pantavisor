b258c60209244ecd60e6adf270ca379a4288237c
Felipe Ortiz <felipe.ortiz@pantacor.com>
2022-06-05 14:42:01 -0400

    Remove irrelevant comments

--- utils/filesystem.h
+++ utils/filesystem.h
@@ -26,95 +26,29 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
-/*
- * Check if the given path exist
- * */
 bool pv_filesystem_path_exist(const char *path);
-/*
- * check if the given path is a directory
- * */
 bool pv_filesystem_path_is_directory(const char *path);
-/*
- * Sync the given path. If path points to a file,
- * then the parent folder is synced
- * */
 void pv_filesystem_path_sync(const char *path);
-/*
- * Path concatenation, the value is returned on the given buffer
- * */
 void pv_filesystem_path_concat(char *buf, int size, ...);
-/*
- * Creates a directory with the given path and mode
- * All subdirectories will be created.
- * */
 int pv_filesystem_mkdir_p(const char *path, mode_t mode);
-/*
- * Remove a path (file or directory). If recursive is true then all
- * subdirectories will be deleted
- * */
 int pv_filesystem_path_remove(const char *path, bool recursive);
-/*
- * Rename a path(file or directory)
- * */
 int pv_filesystem_path_rename(const char *src_path, const char *dst_path);
-/*
- * Returns the file size for the given path
- * */
 size_t pv_filesystem_path_get_size(const char *path);
-/*
- * Creates a temporary path based on the given fname
- * */
 int pv_filesystem_file_tmp(char *tmp, const char *fname);
-/* Save the given data on fname using a specific mode.
- * If the file doesn't exist, will be created.
- * If file exists, it will be truncated.
- * */
 int pv_filesystem_file_save(const char *fname, const char *data, mode_t mode);
-/*
- * Copy a file from src to dst.
- * */
 int pv_filesystem_file_copy_from_path(const char *src, const char *dst, mode_t mode);
-/*
- * Copy the contents of the src file descriptor to dst. If close_src is true
- * the file pointed by src will be closed.
- * */
+
+// sync function should be called after copy
 ssize_t pv_filesystem_file_copy_fd(int src, int dst, bool close_src);
-/*
- * Get an extended attribute from the given file.
- * NOTE: This function recives a buffer (value) where the attribute
- * will be write, so is important to provide a buffer with the right size.
- * */
+
+// Returns 0 on success -1 on error. Check errno for details
 int pv_filesystem_file_get_xattr(char *value, size_t size, const char *fname, const char *attr);
-/*
- * Set an extended attribute in the given file
- * */
 int pv_filesystem_file_set_xattr(const char *fname, const char *attr, const char *value);
-/*
- * Write the given buffer in the file pointed by fd.
- * NOTE: this function blocks until the file is completely written.
- * */
 ssize_t pv_filesystem_file_write_nointr(int fd, const char *buf, ssize_t size);
-/*
- * Reads into the given buffer from the file pointed by fd.
- * NOTE: this function blocks until the file completely read.
- * */
 ssize_t pv_filesystem_file_read_nointr(int fd, char *buf, ssize_t size);
-/*
- * Lock the given file descriptor
- * */
 int pv_filesystem_file_lock(int fd);
-/*
- * Unlock the given file descriptor
- * */
 int pv_filesystem_file_unlock(int fd);
-/*
- * Gzip the given file
- * */
 int pv_filesystem_file_gzip(const char *fname, const char *target_name);
-/*
- * Checks if the given file exists and open it usign the flags and mode
- * provided.
- * */
 int pv_filesystem_file_check_and_open(const char *fname, int flags, mode_t mode);
 
 #endif
