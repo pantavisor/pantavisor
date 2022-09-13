262551fd500dcfe0c3d4f70726b78c324c1e9b82
Felipe Ortiz <felipe.ortiz@pantacor.com>
2022-09-13 13:54:56 -0300

    Remove call to pv_logserver_stop when is a non-reboot update

--- pantavisor.c
+++ pantavisor.c
@@ -222,7 +222,6 @@ static pv_state_t _pv_run(struct pantavisor *pv)
 		// for non-reboot updates...
 		pv_log(INFO, "transitioning...");
 
-		pv_logserver_stop();
 		ph_logger_stop_lenient();
 		ph_logger_stop_force();
 
