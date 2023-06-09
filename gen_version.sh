02467ea510679f41bdc1b7966ef3ff89879c05a9
Alexander Sack <asac@pantacor.com>
2023-06-09 10:19:05 +0200

    make gen_version.sh produce output for CMAKE and PVALCHEMY buildsystem
    Repurpose unused command parameter $1 as type and pass PVALCHEMY as
    argument in atom.mk

--- gen_version.sh
+++ gen_version.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+type=$1
 buildir=$2
 
 IFS=
-manifest=$(.repo/repo/repo manifest -r)
-gitdescribe=$(cd .repo/manifests; git describe --tags)
-sha256=$(echo $manifest | sha256sum | awk '{print $1}')
-manifest=$(echo $manifest | sed 's/"/\\"/g' | sed -e '$ ! s/$/ \\n \\/')
-
 now=$(date +'%y%m%d')
 
-echo "const char *pv_build_manifest = \"${manifest}\";" > $buildir/version.c
-echo "const char *pv_build_version = \"${gitdescribe}-${now}-${sha256:0:7}\";" >> $buildir/version.c
-echo "const char *pv_build_date = \"${now}\";" >> $buildir/version.c
-echo "const char *pv_build_arch = \"${3}\";" >> $buildir/version.c
+if [ "$type" = CMAKE ]; then
+    gitdescribe=$(git describe --tags)
+
+    echo "const char *pv_build_manifest = \"\";" > $buildir/version.c
+    echo "const char *pv_build_version = \"${gitdescribe}-${now}\";" >> $buildir/version.c
+    echo "const char *pv_build_date = \"${now}\";" >> $buildir/version.c
+    echo "const char *pv_build_arch = \"${3}\";" >> $buildir/version.c
+
+elif [ "$type" = PVALCHEMY ]; then
+    manifest=$(.repo/repo/repo manifest -r)
+    gitdescribe=$(cd .repo/manifests; git describe --tags)
+    sha256=$(echo $manifest | sha256sum | awk '{print $1}')
+    manifest=$(echo $manifest | sed 's/"/\\"/g' | sed -e '$ ! s/$/ \\n \\/')
+
+    echo "const char *pv_build_manifest = \"${manifest}\";" > $buildir/version.c
+    echo "const char *pv_build_version = \"${gitdescribe}-${now}-${sha256:0:7}\";" >> $buildir/version.c
+    echo "const char *pv_build_date = \"${now}\";" >> $buildir/version.c
+    echo "const char *pv_build_arch = \"${3}\";" >> $buildir/version.c
+
+else
+    echo "ERROR: unknown build system type for gen_version.sh: $type"
+    exit 128
+fi
 
 echo "version.c generated like:"
 cat $buildir/version.c
