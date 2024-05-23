905f44b9a03f4424f7fc575f8b4795bd62b505a1
Alexander Sack <asac@pantacor.com>
2024-05-23 21:31:00 +0000

    Switch build system to CMake for alchemy and yocto
    install trust certs from PANTAVISOR_PVS_DIR
    
    CMake Flags:
      PANTAVISOR_DEBUG=ON - passes -DPANTAVISOR_DEBUG to build
      PANTAVISOR_DM_VERITY=ON - enable dm verity feature
      PANTAVISOR_DM_CRYPT=ON - enable dm crypt disk feature
      PANTAVISOR_TESTS=ON - build tests
    
    atom.mk:
      - move all build targets to BUILD_CMAKE or BUILD_CUSTOM (for dummy support)
      - use CONFIG_ALCHEMY_BUILD_INIT_DM to enable PANTAVISOR_DM_VERITY feature flag
      - use CONFIG_ALCHEMY_BUILD_INIT_CRYPT to enable PANTAVISOR_DM_CRYPT feature flag
      - introduce pvtests module to build pantavisor including tests
    
    Create skeleton dirs at runtime
      - mkdir /tmp and early mount dirs
      - install scripts with 0755 permissions

--- atom.mk
+++ atom.mk
@@ -2,136 +2,32 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_LIBRARIES := lxc libthttp
-LOCAL_CONDITIONAL_LIBRARIES := OPTIONAL:libseccomp OPTIONAL:libcap OPTIONAL:apparmor
-LOCAL_DESTDIR := ./lib/
+## backward compatility modules (null op) to keep old global.config's happy
 LOCAL_MODULE := pv_lxc
 
-LOCAL_CFLAGS := -g -Wno-format-nonliteral -Wno-format-contains-nul -fPIC
-LOCAL_LDFLAGS := -Wl,--no-as-needed -lutil -Wl,--as-needed
-
-LOCAL_SRC_FILES := plugins/pv_lxc.c utils/fs.c utils/tsh.c utils/pvsignals.c
-
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_CUSTOM)
 
 include $(CLEAR_VARS)
 
-LOCAL_LIBRARIES := libthttp mbedtls picohttpparser zlib
-LOCAL_CONDITIONAL_LIBRARIES := OPTIONAL:e2fsprogs
-
-LOCAL_DESTDIR := ./
 LOCAL_MODULE := init
-
-LOCAL_CFLAGS := -g -Wno-format-nonliteral -Wno-format-contains-nul -D_FILE_OFFSET_BITS=64 -Werror
-LOCAL_LDFLAGS := -Wl,--no-as-needed -ldl -Wl,--as-needed
-
-PV_BUILD_DIR := $(call local-get-build-dir)
-PV_VERSION_C := $(PV_BUILD_DIR)/version.c
-
-$(PV_VERSION_C): .FORCE
-	$(Q) $(PRIVATE_PATH)/gen_version.sh PVALCHEMY $(PV_BUILD_DIR) $(TARGET)
-
-LOCAL_PREREQUISITES += \
-	$(PV_VERSION_H)
-
-ifeq ($(PANTAVISOR_DEBUG), yes)
-LOCAL_CFLAGS += -DPANTAVISOR_DEBUG
-LOCAL_DEPENDS_MODULES += dropbear-pv
-endif
-
+LOCAL_DESCRIPTION := Pantavisor
+LOCAL_LIBRARIES := libthttp mbedtls picohttpparser zlib lxc
+LOCAL_CONDITIONAL_LIBRARIES := OPTIONAL:e2fsprogs OPTIONAL:dropbear-pv OPTIONAL:cryptsetup
+LOCAL_CMAKE_CONFIGURE_ARGS += "-DPANTAVISOR_DM_VERITY=$(CONFIG_ALCHEMY_BUILD_INIT_DM)"
+LOCAL_CMAKE_CONFIGURE_ARGS += "-DPANTAVISOR_DM_CRYPT=$(CONFIG_ALCHEMY_BUILD_INIT_CRYPT)"
 LOCAL_CODECHECK_C := clang
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/utils/
+ifeq ($(CONFIG_ALCHEMY_BUILD_PVZRAM), y)
+LOCAL_CMAKE_CONFIGURE_ARGS += "-DPANTAVISOR_PVZRAM_TEST=ON"
+endif
 
-LOCAL_SRC_FILES := debug.c \
-			daemons.c \
-			init.c \
-			loop.c \
-			logger.c \
-			log.c \
-			logserver/logserver_timestamp.c \
-			logserver/logserver_utils.c \
-			logserver/logserver_out.c \
-			logserver/logserver_update.c \
-			logserver/logserver_singlefile.c \
-			logserver/logserver_filetree.c \
-			logserver/logserver_null.c \
-			logserver/logserver_stdout.c \
-			logserver/logserver.c \
-			config_parser.c \
-			config.c \
-			pantavisor.c \
-			state.c \
-			group.c \
-			condition.c \
-			platforms.c \
-			addons.c \
-			volumes.c \
-			disk/disk.c \
-			disk/disk_crypt.c \
-			disk/disk_swap.c \
-			disk/disk_volume.c \
-			disk/disk_zram.c \
-			disk/disk_utils.c \
-			disk/disk_zram_utils.c \
-			signature.c \
-			parser/parser.c \
-			parser/parser_multi1.c \
-			parser/parser_system1.c \
-			objects.c \
-			utils/tsh.c \
-			utils/system.c \
-			utils/str.c \
-			utils/strrep.c \
-			utils/json.c \
-			utils/base64.c \
-			utils/math.c \
-			utils/timer.c \
-			utils/fs.c \
-			utils/socket.c \
-			utils/pvsignals.c \
-			utils/pvzlib.c \
-			jsons.c \
-			pantahub.c \
-			updater.c \
-			bootloader.c \
-			trestclient.c \
-			uboot.c \
-			rpiab.c \
-			grub.c \
-			storage.c \
-			metadata.c \
-			ctrl.c \
-			wdt.c \
-			network.c \
-			pvlogger.c \
-			pvctl_utils.c \
-			mount.c \
-			ph_logger.c \
-			buffer.c \
-			blkid.c \
-			paths.c \
-			drivers.c \
-			cgroup.c \
-			apparmor.c
+ifeq ($(PANTAVISOR_DEBUG), yes)
+LOCAL_CMAKE_CONFIGURE_ARGS += "-DPANTAVISOR_DEBUG=ON"
+else
+LOCAL_CMAKE_CONFIGURE_ARGS += "-DPANTAVISOR_DEBUG=OFF"
+endif
 
-LOCAL_INSTALL_HEADERS := log.h
-LOCAL_GENERATED_SRC_FILES := version.c
-
-LOCAL_COPY_FILES := $(foreach a,$(shell cd $(LOCAL_PATH)/skel; find . -type f), skel/$(a):$(a)) \
-	scripts/pv_e2fsgrow:lib/pv/pv_e2fsgrow \
-	scripts/hooks_lxc-mount.d/export.sh:lib/pv/hooks_lxc-mount.d/export.sh \
-	scripts/hooks_lxc-mount.d/mdev.sh:lib/pv/hooks_lxc-mount.d/mdev.sh \
-	scripts/hooks_lxc-mount.d/remount.sh:lib/pv/hooks_lxc-mount.d/remount.sh \
-	scripts/JSON.sh:lib/pv/JSON.sh \
-	scripts/pvcrash:lib/pv/pvcrash \
-	tools/pventer:usr/bin/pventer \
-	tools/fallbear-cmd:usr/bin/fallbear-cmd \
-	pvs/trust/ca-certificates.crt:etc/pantavisor/pvs/trust/ca-certificates.crt \
-	pvs/trust/cacerts.default.pem:etc/pantavisor/pvs/trust/cacerts.default.pem \
-	defaults/groups.json:etc/pantavisor/defaults/groups.json
-
-include $(BUILD_EXECUTABLE)
+include $(BUILD_CMAKE)
 
 include $(CLEAR_VARS)
 
@@ -180,30 +76,24 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 
-LOCAL_DESTDIR := ./
+# keep this as null-op target for backward compatibilityyy
 LOCAL_MODULE := init-dm
-LOCAL_LIBRARIES := cryptsetup
-
-LOCAL_COPY_FILES := scripts/volmount/verity/dm:lib/pv/volmount/verity/dm
 
 include $(BUILD_CUSTOM)
 
 include $(CLEAR_VARS)
 
+# keep this as null-op target for backward compatibilityyy
 LOCAL_MODULE := init-crypt
-LOCAL_LIBRARIES := cryptsetup
-LOCAL_COPY_FILES := scripts/volmount/crypt/crypt:lib/pv/volmount/crypt/crypt
 
 include $(BUILD_CUSTOM)
 
 include $(CLEAR_VARS)
 
 LOCAL_DESTDIR := ./
-LOCAL_MODULE := pvzram
+LOCAL_MODULE := pvtests
+LOCAL_LIBRARIES += argp-standalone
+LOCAL_CMAKE_CONFIGURE_ARGS += "-DPANTAVISOR_TESTS=ON"
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
+include $(BUILD_CMAKE)
 
-LOCAL_SRC_FILES := disk/disk_zram_utils.c \
-		   utils/pv_zram.c
-
-include $(BUILD_EXECUTABLE)
