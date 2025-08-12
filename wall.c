da67cb1f7bb0ca2e917bd0eea8d37a3e1b95db8b
Fernando Luiz Cola <fernando.luiz@pantacor.com>
2025-08-12 13:58:32 -0300

    fix: add debug shell in the early boot

--- wall.c
+++ wall.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2025 Pantacor Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,6 +30,7 @@
 
 #include "log.h"
 #include "version.h"
+#include "state.h"
 
 #define MODULE_NAME "wall"
 #define pv_log(level, msg, ...) vlog(MODULE_NAME, level, msg, ##__VA_ARGS__)
@@ -68,6 +91,8 @@ void pv_wall_ssh_users(const char *message, ...)
 
 void pv_wall_banner()
 {
+	struct pantavisor *pv = pv_get_instance();
+
 	pv_wall("______           _              _                ");
 	pv_wall("| ___ \\         | |            (_)               ");
 	pv_wall("| |_/ /_ _ _ __ | |_ __ ___   ___ ___  ___  _ __ ");
@@ -76,6 +101,7 @@ void pv_wall_banner()
 	pv_wall("\\_|  \\__,_|_| |_|\\__\\__,_| \\_/ |_|___/\\___/|_|   ");
 	pv_wall("                                                 ");
 	pv_wall("Pantavisor (TM) (%s) - pantavisor.io", pv_build_version);
+	pv_wall("cmdline: %s", pv->cmdline);
 	pv_wall("                                                 ");
 }
 
