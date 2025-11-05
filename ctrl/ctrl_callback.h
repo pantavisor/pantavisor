6617c93234bf44c7695600e710fb81de28e96cb2
Felipe Ortiz <felipe.ortiz@pantacor.com>
2025-11-05 11:51:58 -0300

    feat: add callback base module

---
+++ ctrl/ctrl_callback.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025 Pantacor Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef PV_CTRL_CALLBACK_H
+#define PV_CTRL_CALLBACK_H
+
+#include "ctrl_caller.h"
+#include "utils/list.h"
+
+#include <stdbool.h>
+
+struct evhttp_request;
+
+typedef void (*pv_ctrl_fn)(struct evhttp_request *, void *);
+
+struct pv_ctrl_cb {
+	char *uri;
+	pv_ctrl_fn fn;
+	int methods;
+	bool need_mgmt;
+	bool upload;
+	struct dl_list lst;
+};
+
+struct pv_ctrl_cb *pv_ctrl_cb_new(const char *path, const int methods,
+				  bool mgmt, pv_ctrl_fn fn);
+
+void pv_ctrl_cb_free(struct pv_ctrl_cb *cb);
+
+#endif
