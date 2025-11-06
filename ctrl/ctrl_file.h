653e149eed50bc763520871fa54037187b2b4dfa
Felipe Ortiz <felipe.ortiz@pantacor.com>
2025-11-06 16:18:32 -0300

    feat: add functions to create updloads and downloads

---
+++ ctrl/ctrl_file.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025 Pantacor Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef PV_CTRL_FILE_H
+#define PV_CTRL_FILE_H
+
+#include <linux/limits.h>
+
+struct evhttp_request;
+
+struct pv_ctrl_file {
+	int fd;
+	char path[PATH_MAX];
+	struct evhttp_request *req;
+};
+
+enum pv_ctrl_file_type { PV_CTRL_FILE_READ, PV_CTRL_FILE_WRITE };
+
+struct pv_ctrl_file *pv_ctrl_file_new(struct evhttp_request *req,
+				      const char *path,
+				      enum pv_ctrl_file_type type);
+
+void pv_ctrl_file_free(struct pv_ctrl_file *file);
+
+#endif
