59e3964c5237f6e2a01d26c9c3e634f469bf4f5d
Aníbal Portero Hermida <anibal.portero@pantacor.com>
2026-06-10 10:19:28 +0200

    feat(pvtest): structured logging format, lifecycle messages and PS4 trace
    - Standardise log format across pvtest-run and pv-appengine:
      [pvtest] <epoch> LEVEL -- [source]: message
    - Add lifecycle log messages (first boot, storage init, SIGINT, stop)
    - Add verbose mode: VERBOSE=true enables set -x in pv-appengine
    - Inject PS4 trace into test scripts for structured set -x output:
      [pvtest] $(date +%s) DEBUG -- [test-script]: command
    - Fix eval_test filter to handle bash's nested PS4 bracket replication

--- disk/disk_volume_bind.c
+++ disk/disk_volume_bind.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Pantacor Ltd.
+ * Copyright (c) 2026 Pantacor Ltd.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
