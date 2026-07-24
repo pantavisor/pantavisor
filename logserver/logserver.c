c5721b9d0fb9e6b635ec56c7f0963cee867cd946
Aníbal Portero Hermida <anibal.portero@pantacor.com>
2026-07-24 11:36:24 +0200

    feat(logserver): send lxc logs to stdout_direct when active

--- logserver/logserver.c
+++ logserver/logserver.c
@@ -57,6 +57,7 @@
 #include "config.h"
 
 #include "pantavisor.h"
+#include "platforms.h"
 #include "buffer.h"
 #include "paths.h"
 #include "config.h"
@@ -610,6 +611,11 @@ static void logserver_consume_fd(int fd)
 		snprintf(d.plat, LOGSERVER_PLAT_MAX_LEN, "%s", lfd->platform);
 
 		logserver_log_msg_data(&d, 0);
+		if ((logserver.active_out & LOG_SERVER_OUTPUT_STDOUT_DIRECT) &&
+		    d.src &&
+		    (!strcmp(d.src, PV_PLATFORM_LXC_LOG) ||
+		     !strcmp(d.src, PV_PLATFORM_LXC_CONSOLE_LOG)))
+			logserver_utils_stdout(&d);
 	} else if (errno != EAGAIN) {
 		pv_log(DEBUG,
 		       "dead fd subscribed found (%d) trying to read: %s",
